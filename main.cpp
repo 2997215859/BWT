e7972519be86f4bdb51f0ee8cc8f6a70ab7a53bf
ruiy <2997215859@qq.com>
2018-05-09 20:29:21 +0800

    Update main.cpp
    test: 修改测试字符串...

--- main.cpp
+++ main.cpp
@@ -12,8 +12,7 @@ int main () {
     const char end_str = '$';
 
 //    std::string origin("abaaba");
-    std::string origin("Tomorrow and tomorrow and tomorrow and tomorrow");
-//    std::string origin("Tomorrow and tomorrow");
+    std::string origin("cpp primer and effective cpp and modern effective cpp and effective stl");
 
     std::string compression_res = compression(origin);
     printf("compression res = %s\n", compression_res.c_str());
